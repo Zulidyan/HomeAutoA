5cbe9246019936745281dd737a8225ee88dd6c6b
ccrraazzyyman <milammatthewl@gmail.com>
2015-04-21 18:50:25 -0500

    Changed Lights.java and added an ID to ToggleDevice so we can keep track of things.

--- app/src/main/java/group2/testapp1/Lights.java
+++ app/src/main/java/group2/testapp1/Lights.java
@@ -1,8 +1,10 @@
 package group2.testapp1;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
+import android.view.View;
 import android.widget.ScrollView;
 import android.widget.TextView;
 import android.widget.ToggleButton;
@@ -16,12 +18,31 @@ public class Lights extends ActionBarActivity {
         super.onCreate(savedInstanceState);
 
         Log.d("", "Inside Lights.class");
+        //create intent
+        Intent intent = getIntent();
+        //String message = intent.getStringExtra(MyActivity.EXTRA_MESSAGE);*/
+        //create textView
+        /*TextView textView = new TextView(this);
+        textView.setTextSize(20);
+        textView.setText("test");*/
+        //set the view
         ScrollView sv1 = new ScrollView(this);
         ToggleButton l1 = new ToggleButton(this);
-        l1.setText("light1");
+        //l1.setText("light1");
+        l1.setTextOff("Light 1 Off");
+        l1.setTextOn("Light 1 On");
+        l1.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view){
+                toggleLight(1);
+            }
+        });
         sv1.addView(l1);
         setContentView(sv1);
 
         //setContentView(R.layout.activity_display_message);
+    }
+    protected void toggleLight(int i){
+        //TODO implement turning light on and off
+        Log.d("","Trying to turn on/off "+i);
     }
 }
--- app/src/main/java/group2/testapp1/ToggleDevice.java
+++ app/src/main/java/group2/testapp1/ToggleDevice.java
@@ -5,19 +5,19 @@ import android.bluetooth.BluetoothSocket;
 public class ToggleDevice extends Device {
 
 	private boolean state = false; //default off
+    public BluetoothSocket mmSocket;
+    private int ID;
 
 	public boolean getState() {
 		return this.state;
 	}
-
     public void setState(boolean state){
         this.state = state;
     }
 
-    public BluetoothSocket mmSocket;
-
-    public ToggleDevice(BluetoothSocket mmSocket){
+    public ToggleDevice(BluetoothSocket mmSocket, int ID){
         this.mmSocket = mmSocket;
+        this.ID = ID;
     }
 
     public void toggleState() {
