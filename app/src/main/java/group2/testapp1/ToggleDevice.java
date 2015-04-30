fa767f439f75220d98cad1748eb357130cf2df04
ccrraazzyyman <milammatthewl@gmail.com>
2015-04-29 21:03:52 -0500

    Fixed a bunch of warnings

--- app/src/main/java/group2/testapp1/ToggleDevice.java
+++ app/src/main/java/group2/testapp1/ToggleDevice.java
@@ -13,7 +13,6 @@ public class ToggleDevice extends Device {
     public void setState(boolean state){
         this.state = state;
     }
-//    public int getId(){return super.getID();}
 
     public ToggleDevice(String name, int id, BluetoothDevice device){
         super(name, id, device);
